468d92df27616989aac28f249f992d40a3fbd427
github username <github email address>
2020-04-20 21:20:31 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,34 @@
 // your Bomb code here!
+import React from 'react';
+class Bomb extends React.Component {
+ 
+    // we use the constructor to set the INITIAL STATE
+    constructor(props) {
+      super()
+      this.state = {
+        secondsLeft: props.initialCount
+      }
+
+      // React.useEffect(() => {
+        this.state.secondsLeft > 0 && setTimeout(this.decrement, 1000);
+      // }, [this.state.secondsLeft]);
+    }
+  
+    decrement = () => {
+      const newCount = this.state.secondsLeft - 1
+      this.setState({
+        secondsLeft: newCount
+      })
+      this.state.secondsLeft > 0 && setTimeout(this.decrement, 1000);
+    }
+
+    render() {
+      return (
+        <div onClick={this.increment}>
+          {this.state.secondsLeft}
+        </div>
+      )
+    }
+  }
+
+  export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,32 @@
 // your ImageSlider code here!
+import React from 'react';
+// import ReactDOM from 'react-dom';
+class ImageSlider extends React.Component {
+ 
+    // we use the constructor to set the INITIAL STATE
+    constructor() {
+      super()
+      this.state = {
+        currentSlideIndex: 0
+      }
+    }
+  
+    // our increment method makes use of the 'setState' method, which is what we use to alter state
+    increment = () => {
+      const newCount = this.state.currentSlideIndex + 1
+      this.setState({
+        currentSlideIndex: newCount
+      })
+    }
+  
+    render() {
+      return (
+        <div onClick={this.increment}>
+          I am on slide {this.state.currentSlideIndex}
+        </div>
+      )
+    }
+  }
+
+
+  export default ImageSlider;
